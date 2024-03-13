ddfe6101301bf8fa5b06a04e0a3a6c936986c49c
nickshakh <shakhbazyann@gmail.com>
2024-03-13 04:01:34 +0400

    feat - just added buttons to the SideNavigation

--- src/app/components/SideNavigation/SideNavigation.tsx
+++ src/app/components/SideNavigation/SideNavigation.tsx
@@ -1,12 +1,47 @@
 import './styles.css';
+import Image from 'next/image';
+import Logo from'./images/Логотип.png'
+import ApplicationImage from'./images/ЗаявкиСВГ.png'
+import EmployeeImage from './images/Сотрудники.png'
+import SettingsImage from './images/Настройки.png'
 
 const SideNavigation = () => {
     return (
         <div className="side-navigation">
-            <div>
-                SideNavigation
-                SideNavigation1
-                SideNavigation2
+
+            <div className='btn-div'>
+            <Image
+            src={Logo}
+            alt="My Image"
+            className='logo-image'
+        />
+        <div className='application-div'>
+            <Image
+            src={ApplicationImage}
+            alt="My Image"
+            className='logo-image'
+            />
+            <button className='application-btn'>Заявки</button>
+        </div>
+
+        <div className='employee-div'>
+            <Image
+            src={EmployeeImage}
+            alt="My Image"
+            className='employee-image'
+            />
+            <button className='employee-btn'>Сотрудники</button>
+        </div>
+
+        <div className='settings-div'>
+            <Image
+            src={SettingsImage}
+            alt="My Image"
+            className='setting-image'
+            />
+            <button className='setting-btn'>Настройки</button>
+        </div>
+
             </div>
         </div>
     )
