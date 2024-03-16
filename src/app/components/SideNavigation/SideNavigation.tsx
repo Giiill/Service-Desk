5effd47a2cedd1ddedb45e7cef6a340ba3bf163a
nickshakh <shakhbazyann@gmail.com>
2024-03-16 13:32:34 +0400

    Added Link tag for sidebar,sizes for the images in mobile

--- src/app/components/SideNavigation/SideNavigation.tsx
+++ src/app/components/SideNavigation/SideNavigation.tsx
@@ -4,6 +4,7 @@ import Logo from'./images/Logo.svg'
 import ApplicationImage from'./images/Application.svg'
 import EmployeeImage from './images/Employees.svg'
 import SettingsImage from './images/Settings.svg'
+import Link from 'next/link';
 
 const SideNavigation = () => {
     return (
@@ -15,27 +16,33 @@ const SideNavigation = () => {
             className='logo-image'
             />
         <div className='application-div'>
+            <Link href={'/'}>
             <Image
             src={ApplicationImage}
             alt="My Image"
             className='application-image'
             />
+            </Link>
         </div>
 
         <div className='employee-div'>
+            <Link href={'/'}>
             <Image
             src={EmployeeImage}
             alt="My Image"
             className='employee-image'
             />
+            </Link>
         </div>
 
         <div className='settings-div'>
+            <Link href={'/'}>
             <Image
             src={SettingsImage}
             alt="My Image"
             className='setting-image'
             />
+            </Link>
         </div>
 
             </div>
