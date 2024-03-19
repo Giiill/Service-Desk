10e90bd3154ed641ac458e4c7a24120a38057f4b
nickshakh <shakhbazyann@gmail.com>
2024-03-20 03:58:49 +0400

    Fix - fixed TopBar user info, created Link

--- src/app/components/TopBar/TopBar.tsx
+++ src/app/components/TopBar/TopBar.tsx
@@ -1,13 +1,16 @@
 import './styles.css';
 import Image from 'next/image';
 import MainImage from './images/main-picture.jpg'
+import Link from 'next/link';
+
+
 const TopBar = () => {
     return (
         <div className="top-bar-main">
             <div className="top-bar-searching">
             <input type="text"  className='top-bar-input'placeholder='Тут должна быть лупа'/>
             </div>
-
+                <Link href={'/'}>
             <div className="user-info-div">
                 <div className="name-surname-div">
                 <h1>Name</h1>
@@ -21,6 +24,7 @@ const TopBar = () => {
                 height={45}
             />
             </div>
+            </Link>
         </div>
     )
 };
