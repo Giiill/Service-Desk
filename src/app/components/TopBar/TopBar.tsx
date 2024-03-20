80760056d3d3c94ff9da0fc0efa4305467bd4731
nickshakh <shakhbazyann@gmail.com>
2024-03-20 17:38:27 +0400

    Added loupe

--- src/app/components/TopBar/TopBar.tsx
+++ src/app/components/TopBar/TopBar.tsx
@@ -2,13 +2,20 @@ import './styles.css';
 import Image from 'next/image';
 import MainImage from './images/main-picture.jpg'
 import Link from 'next/link';
-
+import LoupeImage from './images/loupe.svg'
 
 const TopBar = () => {
     return (
         <div className="top-bar-main">
             <div className="top-bar-searching">
-            <input type="text"  className='top-bar-input'placeholder='Тут должна быть лупа'/>
+            <input type="text" className='top-bar-input'/>
+            <Image
+            src={LoupeImage}
+            alt='Searching'
+            width={30}
+            height={25}
+            className='loupe-input'
+            />
             </div>
                 <Link href={'/'}>
             <div className="user-info-div">
